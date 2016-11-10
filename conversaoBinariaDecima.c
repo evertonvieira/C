f4f540e807f4819baacee84b0481576ecb57bf55
Everton <evertonvieira1994@gmail.com>
2016-11-10 13:46:56 -0200

    Atualizando exercio de conversão binaria

--- conversaoBinariaDecima.c
+++ conversaoBinariaDecima.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-#define MAX 10000
 
-int main(){
+int main()
+{
 
-	int  c, d, proximo, i = 0;
-	int atual;
+	int d, proximo, atual;
+	long unsigned int  c;
 	atual = getchar();
+	proximo = getchar();
+	c = (atual - '0')*2 + (proximo - '0');
+	printf("2x%d + %d = %d\n", (atual - '0'), (proximo - '0'), c );
 	while ((proximo = getchar()) != '\n'){
-		if (!i){
-			c = (atual - '0')*2 + (proximo - '0');
-			i++;
-			printf("%d - ", c);
-		}else{
-			c = 2*c + (proximo - '0');
-			printf("%d - ", c);
-		}
+		int d = c*2 + (proximo - '0');
+		printf("2x%d + %d = %d\n", c, (proximo - '0'), d );
+		c = 2*c + (proximo - '0');
 	}
-	printf("\n");
+	printf("%ld\n", c);
 	return 0;
+
 }
+
